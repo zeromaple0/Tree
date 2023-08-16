020c2704944609f83f8757a627379299aaa06577
zeromaple0 <139435479+zeromaple0@users.noreply.github.com>
2023-08-16 12:19:17 +0800

    Add files via upload

---
+++ BinaryTree/Tree.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public class Tree<TItem> where TItem : IComparable<TItem>
+    {
+        public TItem NodeData { get; set; }
+        public Tree<TItem> LeftTree { get; set; }
+        public Tree<TItem> RightTree { get; set; }
+
+        public Tree(TItem nodeValue)
+        {
+            NodeData = nodeValue;
+            LeftTree = null;
+            RightTree = null;
+        }
+        public void Insert(TItem newItem)
+        {
+            TItem currentNodeValue = NodeData;
+            if (currentNodeValue.CompareTo(newItem) > 0)
+            {
+                //将新项插入左子树
+                if (LeftTree == null)
+                {
+                    LeftTree = new Tree<TItem>(newItem);
+                }
+                else
+                {
+                    LeftTree.Insert(newItem);
+                }
+            }
+            else
+            {
+                //将新项插入右子树
+                if (RightTree == null)
+                {
+                    RightTree = new Tree<TItem>(newItem);
+                }
+                else
+                {
+                    RightTree.Insert(newItem);
+                }
+            }
+        }
+        public string WalkTree()//遍历
+        {
+            string result = "";
+            if (LeftTree != null)
+            {
+                result = LeftTree.WalkTree();
+            }
+            result += $" {NodeData.ToString()}";
+
+            if (RightTree != null)
+            {
+                result += RightTree.WalkTree();
+            }
+            return result;
+        }
+    }
+}
